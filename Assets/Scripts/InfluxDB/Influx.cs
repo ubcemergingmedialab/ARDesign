3c31546e8371b3e14d6b0ad82a2d5b07d20b223f
unoctium1 <unoctium1@gmail.com>
2019-04-11 19:39:18 -0700

    Moved namespace comments to seperate files

---
+++ Assets/Scripts/InfluxDB/Influx.cs
@@ -0,0 +1,8 @@
+﻿namespace ARDesign
+{
+    /// <summary>
+    /// Handles all database fetching related functions
+    /// \todo Eventually we should move database stuff onto an external server, these operations are too bulky and slow for the hololens
+    /// </summary>
+    namespace Influx { };
+}
